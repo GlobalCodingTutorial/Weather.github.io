c64f4547d499597bd31f81463a229b2411b179ee
Vkgola <vgola261@gmail.com>
2024-12-14 13:32:02 +0530

    first change

---
+++ script.js
@@ -0,0 +1,168 @@
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
+
+const API_KEY = "27d3619ba6e2ded6784993e4b68a1391"; // API key for OpenWeatherMap API
+
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if(index === 0) { // HTML for the main weather card
+        return `<div class="details">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity} M/S</h6>
+                    <h6>Pressure: ${weatherItem.main.pressure}Pa</h6>
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>${weatherItem.weather[0].description}</h6>
+                </div>`;
+    } else { // HTML for the other five day forecast card
+        return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                    <h6>Pressure: ${weatherItem.main.pressure}Pa</h6>
+                </li>`;
+    }
+}
+
+const getWeatherDetails = (cityName, latitude, longitude) => {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
+        // Filter the forecasts to get only one forecast per day
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForecastDays.push(forecastDate);
+            }
+        });
+
+        // Clearing previous weather data
+        cityInput.value = "";
+        currentWeatherDiv.innerHTML = "";
+        weatherCardsDiv.innerHTML = "";
+
+        // Creating weather cards and adding them to the DOM
+        fiveDaysForecast.forEach((weatherItem, index) => {
+            const html = createWeatherCard(cityName, weatherItem, index);
+            if (index === 0) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", html);
+            } else {
+                weatherCardsDiv.insertAdjacentHTML("beforeend", html);
+            }
+        });        
+    }).catch(() => {
+        alert("An error occurred while fetching the weather forecast!");
+    });
+}
+
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if (cityName === "") return;
+    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    
+    // Get entered city coordinates (latitude, longitude, and name) from the API response
+    fetch(API_URL).then(response => response.json()).then(data => {
+        if (!data.length) return alert(`No coordinates found for ${cityName}`);
+        const { lat, lon, name } = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch(() => {
+        alert("An error occurred while fetching the coordinates!");
+    });
+}
+
+const getUserCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+            const { latitude, longitude } = position.coords; // Get coordinates of user location
+            // Get city name from coordinates using reverse geocoding API
+            const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            fetch(API_URL).then(response => response.json()).then(data => {
+                const { name } = data[0];
+                getWeatherDetails(name, latitude, longitude);
+            }).catch(() => {
+                alert("An error occurred while fetching the city name!");
+            });
+        },
+        error => { // Show alert if user denied the location permission
+            if (error.code === error.PERMISSION_DENIED) {
+                alert("Geolocation request denied. Please reset location permission to grant access again.");
+            } else {
+                alert("Geolocation request error. Please reset location permission.");
+            }
+        });
+}
+
+locationButton.addEventListener("click", getUserCoordinates);
+searchButton.addEventListener("click", getCityCoordinates);
+cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
+
+
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('time').textContent = timeString;
+  }
+  
+  setInterval(updateTime, 1000); // Update time every second);
+  
+  
+  
+  
+  const calendar = document.getElementById('calendar');
+  
+  function displayCalendar() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+    const currentDay = currentDate.getDate();
+    const currentYear = currentDate.getFullYear();
+    const currentWeekday = currentDate.toLocaleString('default', { weekday: 'long' });
+  
+    const dateDiv = document.createElement('div');
+    dateDiv.classList.add('date');
+    dateDiv.textContent = currentWeekday + ', ' + currentMonth + ' ' + currentDay + ', ' + currentYear;
+    calendar.appendChild(dateDiv);
+  }
+  displayCalendar();
+
+
+  
+    
+// Temperature converter function (Celsius to Kelvin)
+function convertTemperature() {
+    const celsius = parseFloat(document.getElementById('temperature').value);
+    const kelvin = celsius + 273.15;
+    document.getElementById('convertedTemperature').textContent = kelvin.toFixed(2) + ' K';
+  }
+  
+  // Wind speed converter function (m/s to km/h)
+  function convertWindSpeed() {
+    const metersPerSecond = parseFloat(document.getElementById('windSpeed').value);
+    const kilometersPerHour = metersPerSecond * 3.6;
+    document.getElementById('convertedWindSpeed').textContent = kilometersPerHour.toFixed(2) + ' km/h';
+  }
+  
+  // Humidity converter function (% to g/m^3)
+  function convertHumidity() {
+    const humidityPercentage = parseFloat(document.getElementById('humidity').value);
+    const gramsPerCubicMeter = humidityPercentage * 10; // Assuming standard conditions
+    document.getElementById('convertedHumidity').textContent = gramsPerCubicMeter.toFixed(2) + ' g/m³';
+  }
+  
+  // Pressure converter function (Pascal to psi)
+  function convertPressure() {
+    const pascal = parseFloat(document.getElementById('pressure').value);
+    const psi = pascal * 0.000145038;
+    document.getElementById('convertedPressure').textContent = psi.toFixed(2) + ' psi';
+  }
+  
